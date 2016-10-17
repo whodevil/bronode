e28f05309080b2294cdc4d69547d7c1c8c12eaab
whodevil <whodevil@offthecob.info>
2016-10-16 21:10:04 -0700

    fixup merge failure

--- server.js
+++ server.js
@@ -1,10 +1,13 @@
 var express = require('express');
 var app = express();
 
+app.use(express.static('client/build'));
+
 app.get('/hello', function (req, res) {
   res.send('Hello World!adasd');
 });
 
-app.listen(process.env.PORT || 3001, function () {
-  console.log('bronode running on 3001!');
+const port = process.env.PORT || 3001;
+app.listen(port, function () {
+  console.log('bronode running on '+port);
 });
